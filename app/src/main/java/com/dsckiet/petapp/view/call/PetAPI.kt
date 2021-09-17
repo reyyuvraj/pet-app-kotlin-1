3551398daadd5842ec2c07618e07f5de5df15528
reyyuvraj <yuvrajsingh77733999@gmail.com>
2021-09-17 17:24:23 +0530

    gradle changes

--- app/src/main/java/com/dsckiet/petapp/view/call/PetAPI.kt
+++ app/src/main/java/com/dsckiet/petapp/view/call/PetAPI.kt
@@ -5,7 +5,6 @@ import retrofit2.http.*
 interface PetAPI {
 
     companion object {
-        const val doc = "https://documenter.getpostman.com/view/16864988/TzzHksRf"
         const val baseURL = "https://ancient-earth-36601.herokuapp.com"
         const val sampleToken = "gbff0078ajg4vt10oc6s56fp2rprdpze"
     }
@@ -14,11 +13,11 @@ interface PetAPI {
     fun postRegister(
     )
 
-    @POST("/login/register")
+    @POST("/login/login")
     fun postLogin(
     )
 
-    @GET("/login/register")
+    @GET("/login/logout")
     fun getLogout(
         @Header("token")
         token: String = "ok"
--- app/src/main/java/com/dsckiet/petapp/view/view/fragment/SignUpAboutOwnerFragment.kt
+++ app/src/main/java/com/dsckiet/petapp/view/view/fragment/SignUpAboutOwnerFragment.kt
@@ -9,6 +9,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import com.dsckiet.petapp.R
 import com.dsckiet.petapp.databinding.FragmentSignup2Binding
+import kotlinx.android.synthetic.main.fragment_signup2.*
 
 class SignUpAboutOwnerFragment : Fragment() {
     private lateinit var binding: FragmentSignup2Binding
@@ -27,6 +28,24 @@ class SignUpAboutOwnerFragment : Fragment() {
 
         binding.btnNext.setOnClickListener {
             findNavController().navigate(R.id.action_signup2Fragment_to_signup3Fragment)
+        }
+    }
+
+    private fun userSignUp(){
+        val name = binding.etYourName
+        val username = binding.etUsername
+        val email= binding.etEmail
+        val password = binding.etPassword
+        val cPassword = binding.etCnfpassword
+
+        if(password.length() < 6){
+            binding.etPassword.error = "Password should be at least 6 characters long."
+            binding.etPassword.requestFocus()
+        }
+
+        if(cPassword != password){
+            binding.etCnfpassword.error = "Passwords do not match."
+            binding.etCnfpassword.requestFocus()
         }
     }
 }
